559c08371204a4f0ac8a963e4372d192e435a754
Carlos Liu <carlosliu@live.com>
2015-08-01 21:15:07 +1200

    add atom.sh, stop sync atom.sh and brew.sh to HOME directory

---
+++ atom.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+apm install Sublime-Style-Column-Selection atom-beautify block-comment color-picker command-logger docblockr editor-stats editorconfig file-icons highlight-line highlight-selected javascript-snippets last-cursor-position linter linter-coffeelint linter-csslint linter-eslint minimap minimap-find-and-replace monokai pigments react wakatime
