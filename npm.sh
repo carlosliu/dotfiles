a2aebc273d54d4d4583bdc67a1309b9124a6fb43
Carlos Liu <carlosliu@live.com>
2015-08-20 18:22:16 +1200

    add more npm packages

--- npm.sh
+++ npm.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 npm install -g npm@latest
-npm install -g jshint eslint
+npm install -g coffee-script coffeelint
+npm install -g jshint jsonlint csslint js-yaml eslint
 npm install -g babel-eslint eslint-plugin-react
 npm install -g eslint-plugin-angular eslint-config-angular
-npm install -g coffee-script gulp
-npm install -g tern
+npm install -g tern gulp
 
 if [[ -f $HOME/github/typescript-tools/package.json ]]; then
     cd $HOME/github/typescript-tools;
