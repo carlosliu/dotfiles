3104510d0e82c055f4dc80c5fec1e584d3bf7455
Carlos Liu <carlosliu@live.com>
2015-08-23 01:57:06 +1200

    more apm plugin

--- apm.sh
+++ apm.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 apm install \
-	Sublime-Style-Column-Selection atom-beautify block-comment \
-	color-picker command-logger docblockr editor-stats editorconfig \
-	file-icons highlight-line highlight-selected \
-	angularjs javascript-snippets \
-	go-plus \
-	language-docker \
-	language-elixir language-erlang \
-	last-cursor-position term2 \
-	linter linter-coffeelint linter-csslint linter-eslint \
-	minimap minimap-find-and-replace \
-	monokai pigments react wakatime
-
+    Sublime-Style-Column-Selection atom-fuzzy-grep atom-beautify block-comment \
+    color-picker command-logger disable-keybindings docblockr editor-stats editorconfig \
+    file-icons highlight-line highlight-selected \
+    angularjs react javascript-snippets jquery-snippets \
+    go-plus language-docker language-elixir language-erlang \
+    last-cursor-position term2 \
+    linter linter-coffeelint linter-csslint linter-eslint \
+    minimap minimap-find-and-replace \
+    dracula-theme monokai pigments wakatime
