9ab15eff2c4e140eacbd64bfb7e4013b73a80d36
Carlos Liu <carlosliu@live.com>
2015-09-07 13:01:23 +1200

    tweak atom packages

--- apm.sh
+++ apm.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
 apm install \
-    Sublime-Style-Column-Selection atom-fuzzy-grep atom-beautify block-comment \
-    color-picker command-logger disable-keybindings docblockr editor-stats editorconfig \
-    file-icons highlight-line highlight-selected \
-    angularjs react javascript-snippets jquery-snippets \
-    go-plus language-docker language-elixir language-erlang \
-    last-cursor-position term2 \
+    Sublime-Style-Column-Selection \
+    atom-alignment atom-beautify atom-fuzzy-grep block-comment \
+    color-picker command-logger dash disable-keybindings docblockr \
+    editor-stats editorconfig \
+    file-icons gist-it git-blame git-plus \
+    highlight-line highlight-selected last-cursor-position \
+    minimap minimap-find-and-replace pigments term2 wakatime
+
+
+apm install \
+    emmet javascript-snippets jquery-snippets \
+    atom-typescript angularjs react \
     linter linter-coffeelint linter-csslint linter-eslint \
-    minimap minimap-find-and-replace \
-    dracula-theme monokai pigments wakatime
+    go-plus language-docker language-elixir language-erlang
+
+
+apm install dracula-theme monokai
