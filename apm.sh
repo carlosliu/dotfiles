fc43ffacd88eff91bd63263378ffceed75221762
Carlos Liu <carlosliu@live.com>
2015-09-12 19:29:58 +1200

    add atom linter-jshint package

--- apm.sh
+++ apm.sh
@@ -13,7 +13,7 @@ apm install \
 apm install \
     atom-typescript emmet react \
     angularjs angularjs-styleguide-snippets jquery-snippets \
-    linter linter-coffeelint linter-csslint linter-eslint \
+    linter linter-coffeelint linter-csslint linter-eslint linter-jshint \
     go-plus language-docker language-elixir language-erlang
 
 apm install atom-material-ui batman-syntax monokai
