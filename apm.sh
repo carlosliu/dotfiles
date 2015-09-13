6ede1ac247f3857315a975ac5c7d609568e1d3b2
Carlos Liu <carlosliu@live.com>
2015-09-13 20:21:25 +1200

    tweak atom packages

--- apm.sh
+++ apm.sh
@@ -5,19 +5,21 @@ apm install \
     atom-alignment atom-beautify atom-fuzzy-grep autoclose-html \
     block-comment cursor-history \
     color-picker command-logger dash disable-keybindings docblockr \
-    editor-stats editorconfig \
-    file-icons gist-it git-blame git-plus open-on-github \
+    editor-stats editorconfig file-icons \
+    gist-it git-blame git-plus open-on-github \
     highlight-column highlight-line highlight-selected \
-    minimap minimap-find-and-replace pigments term2 wakatime
+    minimap minimap-find-and-replace \
+    open-recent project-manager pigments term2 wakatime zentabs
 
 apm install \
     atom-typescript react \
     angularjs angularjs-styleguide-snippets jquery-snippets \
+    atom-bootstrap3 underscorejs-snippets \
     linter linter-coffeelint linter-csslint linter-eslint linter-jshint \
     go-plus language-docker language-elixir language-erlang
 
 apm install atom-material-ui batman-syntax monokai
 
-apm install vim-mode ex-mode vim-surround
+apm install vim-mode ex-mode vim-surround vim-mode-visual-block
 
 # apm install javascript-snippets dracula-theme
